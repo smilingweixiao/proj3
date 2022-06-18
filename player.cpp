005467449e60f2610a6e9d92a880e539f59ed92d
USER <smilingweixiao@DESKTOP-FPV4G8G.localdomain>
2022-06-19 03:59:47 +0800

    現在四點好想睡覺，寫了一點點計算

--- player.cpp
+++ player.cpp
@@ -20,50 +20,87 @@ int player, opponent;
 const int SIZE = 15;
 std::array<std::array<int, SIZE>, SIZE> board;
 
-//int depth; //initialize ,deeper win
-int dirx[8] = {0,0,1,1,1,-1,-1,-1};
-int diry[8] = {1,-1,0,1,-1,0,1,-1};
+enum DIRCTION {
+    VIRT = 0,
+    HORI = 1,
+    RL = 2,
+    LR = 3
+};
+int dirx[4] = {-1,0,1,1};
+int diry[4] = {0,1,-1,1};
+int dep = 5; //initialize ,deeper win
 vector<int> chess[15];//chess board
 map<int,set<int>> blank;
 
 struct Point {
     int x;
     int y;
+    int dir[4];
+    Point(int x, int y) :x(x),y(y) {
+        dir[0] = dir[1] = dir[2] = dir[3] = 0;
+    }
 };
-
+Point spot;
 
 struct State_Value {
-    int alpha, beta;
+    void update_board(int x, int y, int color) {
+        board[x][y] = color;
+    }
+    void restore_board(int x, int y) {
+        board[x][y] = EMPTY;
+    }
 };
 
 int setHeuristic() {
+    int val;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            Point ij(i, j);
+            for (int k = 0; k < 4; k++) {
+                int new_x = i+2*dirx[k];
+                int new_y = j+2*diry[k];
+                if(ij.dir[k]) continue;
+                if(!board[i][j]) continue;
+                if(new_x>=0 && new_x<SIZE && new_y>=0 && new_y<SIZE) {
+                    val+=1;
+                    
+                }
+                else {
 
+                }
+            }
+        }
+    }
 }
 
 int val_alphabeta(int nx, int ny, int depth, int alpha, int beta, bool user) {
     int val;
     int flag = 0;
     if(depth == 0) return setHeuristic();
-    blank.at(nx).erase(ny);
     if(user) {
-        board[nx][ny] = player;
+        board[nx][ny] = player; //state
         val = INT32_MIN;
         for(auto row : blank) {
             for(auto col : row.second) {
                 val = max(val, val_alphabeta(row.first, col, depth-1, alpha, beta, false));  
-                alpha = max(alpha, val);
+                if(alpha < val && alpha < beta) {    //choose to put
+                    alpha = val;
+                    spot.x = row.first;
+                    spot.y = col; 
+                }
                 if(alpha >= beta) {
                     flag = 1;
                     break;
                 }
-                if(flag) break;
+                
             }
+            if(flag) break;
         }
-        board[nx][ny] = 0;
+        board[nx][ny] = EMPTY;      //state
         return val;
     }
     else {
-        board[nx][ny] = opponent;
+        board[nx][ny] = opponent;  //state
         val = INT32_MAX;
         for(auto row : blank) {
             for(auto col : row.second) {
@@ -73,10 +110,11 @@ int val_alphabeta(int nx, int ny, int depth, int alpha, int beta, bool user) {
                     flag = 1;
                     break;
                 }
-                if(flag) break;
+                
             }
+            if(flag) break;
         }
-        board[nx][ny] = 0;
+        board[nx][ny] = EMPTY;         //state
         return val;
     }
 }
@@ -89,8 +127,8 @@ void read_board(std::ifstream& fin) {
             if(board[i][j]) blank.at(i).erase(j);
         }
     }
-    if(player == 1) opponent = 2;
-    else opponent = 1;
+    if(player == BLACK) opponent = WHITE;
+    else opponent = BLACK;
 }
 
 void write_valid_spot(std::ofstream& fout) {
@@ -102,7 +140,7 @@ void write_valid_spot(std::ofstream& fout) {
         int x = (rand() % SIZE);
         int y = (rand() % SIZE);
         if (board[x][y] == EMPTY) {
-            fout << x << " " << y << std::endl;
+            fout << spot.x << " " << spot.y << std::endl;
             // Remember to flush the output to ensure the last action is written to file.
             fout.flush();
         }
