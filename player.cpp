3dde6f432b6bb49568afaa7d355178be062e436e
USER <smilingweixiao@DESKTOP-FPV4G8G.localdomain>
2022-06-20 03:22:21 +0800

    原來是多fin一行==

--- player.cpp
+++ player.cpp
@@ -20,7 +20,7 @@ std::array<std::array<int, SIZE>, SIZE> board;
 
 int dx[4] = {0,1,1,1};
 int dy[4] = {1,0,1,-1};
-const int dep = 6;
+const int dep = 2;
 
 struct Point {
     int x;
@@ -73,7 +73,7 @@ struct Node {
                     new1_y = j+dy[k]; new2_y = j+dy[k]*2;
                     new3_y = j+dy[k]*3; new4_y = j+dy[k]*4;
                     if(new2_x>=0 && new2_x<SIZE && new2_y>=0 && new2_y<SIZE && bd[i][j] == player) {
-                        if(bd[i][j] == bd[new1_x][new1_y] == bd[new2_x][new2_y]){
+                        if(bd[i][j] == bd[new1_x][new1_y] && bd[i][j] == bd[new2_x][new2_y]){
                             val++;
                             isline[i][j][k] = isline[new1_x][new1_y][k] = isline[new2_x][new2_y][k] = 1;
                             if(new3_x >= 0 && new3_x < SIZE && new3_y >= 0 && new3_y < SIZE && bd[new3_x][new3_y] == bd[i][j]) {
@@ -86,7 +86,7 @@ struct Node {
                         }
                     }
                     else if(new2_x>=0 && new2_x<SIZE && new2_y>=0 && new2_y<SIZE) {
-                        if(bd[i][j] == bd[new1_x][new1_y] == bd[new2_x][new2_y]){
+                        if(bd[i][j] == bd[new1_x][new1_y] && bd[i][j] == bd[new2_x][new2_y]){
                             val--;
                             isline[i][j][k] = isline[new1_x][new1_y][k] = isline[new2_x][new2_y][k] = 1;
                             if(new3_x >= 0 && new3_x < SIZE && new3_y >= 0 && new3_y < SIZE) {
@@ -148,40 +148,48 @@ void read_board(std::ifstream& fin) {
     if(player == BLACK) opponent = WHITE;
     else opponent = BLACK;
 }
-void write_valid_spot(std::ofstream& fout) {
+void write_valid_spot(std::ofstream& fout, std::ifstream& fin) {
     srand(time(NULL));
     int x, y, val;
+    best.x = -1;
+    best.y = -1;
     // Keep updating the output until getting killed.
-    while(true) {
-        best.x = (rand() % SIZE);
-        best.y = (rand() % SIZE);
-        Node root;
+    //while(true) {
+        // Choose a random spot.
+        //int x = (rand() % SIZE);
+        //int y = (rand() % SIZE);
+        val = INT32_MIN;
+        //best.x = (rand() % SIZE);
+        //best.y = (rand() % SIZE);
+        Node root = Node();
         for(int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 if(root.bd[i][j] != EMPTY) continue;
                 val = max(val, alphabeta(Node(root, i, j, player), dep-1, false));
                 if(val > root.alpha) {
                     root.alpha = val;
-                    best.x = i;
-                    best.y = j;
+                    x = i;
+                    y = j;
                 }
                 
             }
         }
-        // Choose a random spot.
-        
-        if (board[best.x][best.y] == EMPTY) {
-            fout << best.x << " " << best.y << std::endl;
-            // Remember to flush the output to ensure the last action is written to file.
-            fout.flush();
-        }
-    }
+        //if(best.x == x && best.y == y) {
+        //    read_board(fin);
+        //    continue;
+        //}
+        best.x = x; best.y = y;
+        fout << x << " " << y << std::endl;
+        // Remember to flush the output to ensure the last action is written to file.
+        fout.flush();
+    return;    
+//    }
 }
 int main(int, char** argv) {
     std::ifstream fin(argv[1]);
     std::ofstream fout(argv[2]);
     read_board(fin);
-    write_valid_spot(fout);
+    write_valid_spot(fout,fin);
     fin.close();
     fout.close();
     return 0;
